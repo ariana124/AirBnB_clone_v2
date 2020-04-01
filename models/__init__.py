1ef0d0f4fb4cd38b8f3661bcb8e16e7da3c22005
ariana124 <ariana.bibiano@gmail.com>
2020-04-01 21:12:46 +0000

    reverted files back to before task 6

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """create a unique FileStorage instance for your application"""
-import os
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.user import User
@@ -11,12 +10,5 @@ from models.place import Place
 from models.review import Review
 
 
-if os.environ.get("HBNB_TYPE_STORAGE") == "db":
-    from models.engine.db_storage import DBStorage
-    storage = DBStorage()
-
-else:
-    from models.engine.file_storage import FileStorage
-    storage = FileStorage()
-
+storage = FileStorage()
 storage.reload()
