e5af7e30b4c1d499eb354fbdac31543351de607c
ariana124 <ariana.bibiano@gmail.com>
2020-04-02 21:35:39 +0000

    changed/removes some imports

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """create a unique FileStorage instance for your application"""
 import os
-from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
