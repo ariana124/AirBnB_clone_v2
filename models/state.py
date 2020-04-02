44f0c432393c007d465bc774835d962fb2d8a3f8
ariana124 <ariana.bibiano@gmail.com>
2020-04-02 18:21:26 +0000

    added relationship to City

--- models/state.py
+++ models/state.py
@@ -2,6 +2,7 @@
 """This is the state class"""
 from models.base_model import BaseModel
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
@@ -12,3 +13,4 @@ class State(BaseModel, Base):
     __tablename__ = "states"
 
     name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
