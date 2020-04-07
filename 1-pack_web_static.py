90805b73f033938d416e84426f3e8f4a7a65250c
ariana124 <ariana.bibiano@gmail.com>
2020-04-07 01:06:49 +0000

    added run to use .failed

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,17 +3,18 @@
 fabric script that generates a .tgz archive
 """
 from datetime import datetime
-from fabric.api import local
+from fabric.api import local, run
 
 
 def do_pack():
     """ generates a .tgz archive from the contents of web_static """
-    try:
-        time = datetime.now().strf("%Y%m%d%H%M%S")
-        archive = "versions/web_static{}.tgz".format(time)
-        local("mkdir -p versions")
-        local("tar -cvzf {} web_static".format(archive))
-        return archive
 
-    except:
+    time = datetime.now().strf("%Y%m%d%H%M%S")
+    archive = "versions/web_static{}.tgz".format(time)
+    local("mkdir -p versions")
+    result = local("tar -cvzf {} web_static".format(archive))
+
+    if result.failed:
         return None
+    else:
+        return archive
