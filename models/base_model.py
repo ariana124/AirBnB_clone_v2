0a156ab198456416a11352aeca550c3f84b164d6
ariana124 <ariana.bibiano@gmail.com>
2020-04-22 23:02:53 +0000

    updates init method

--- models/base_model.py
+++ models/base_model.py
@@ -34,9 +34,14 @@ class BaseModel:
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
-        else:
+                if self.id is None:
+                    setattr(self, 'id', str(uuid.uuid4()))
+        if "id" not in kwargs:
             self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
+        if "created_at" not in kwargs:
+            self.created_at = datetime.now()
+        if "updated_at" not in kwargs:
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """returns a string
